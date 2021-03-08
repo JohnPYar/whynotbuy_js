babc53e3efdd29c7375633e0f9664d04c8ef4fbc
JohnYar <johnpyar@gmail.com>
2021-03-08 11:25:03 +0300

    code ready full

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
     // по умолчанию скрываем способ оплаты для регионов за наличные
     $('label input[value=cash]').parent().hide();
@@ -20,10 +22,40 @@ $(document).ready(function () {
 
 //  проверяем значение города по умолчанию
     $.each(cities, function (index, value) {
-        if (value == city) {
+        if (value === city) {
             $('label input[value=cash]').parent().show();
         }
     })
 
 
+    // отслеживаем выбор город в выпадающем списке, по клику проверяем город
+
+    $(".searchbox-list").click( function () {
+
+        console.log('Click');
+
+        let address = $('.t-input-description').text().split(', ');
+
+        // console.log(address)
+
+        // let city = address[address.length -1];
+        let city = 'г Москва';
+
+        console.log(city)
+
+//  проверяем значение города
+        $.each(cities, function (index, value) {
+
+            console.log(index, value);
+
+            if (value === city) {
+                $('label input[value=cash]').parent().show();
+                return false;
+            } else {
+                $('label input[value=cash]').parent().hide();
+            }
+        })
+    })
+
 })
+
